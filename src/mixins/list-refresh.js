1de81fb7bc31b67625b4c17db6197ca9b85ab941
youjiaxing <287009007@qq.com>
2020-08-22 15:13:25 +0800

    7.6 使用 mixin 优化代码

---
+++ src/mixins/list-refresh.js
@@ -0,0 +1,36 @@
+export default {
+    data: {
+        page: 1,
+        // 数据
+        resourceData: [],
+        // 更多数据可加载
+        noMoreData: false,
+        // 是否加载中
+        isLoading: false
+    },
+    async onPullDownRefresh() {
+        await loadData(true)
+        this.page++
+        this.noMoreData = false
+        wx.stopPullDownRefresh()
+    },
+    async onReachBottom() {
+        if (this.noMoreData || this.isLoading) {
+            return
+        }
+        this.isLoading = true
+        this.page++
+        await this.loadData()
+        this.isLoading = false
+    },
+    methods: {
+        async loadData(reset = false) {
+            const resp = await this.fetchData()
+            const meta = resp.data.meta
+
+            this.resourceData = reset ? resp.data.data : this.resourceData.concat(resp.data.data)
+
+            this.noMoreData = meta.current_page === meta.last_page
+        }
+    }
+}
